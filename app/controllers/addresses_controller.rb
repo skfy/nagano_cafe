a4afae6fbe99472384af582e4beebf5a3c5ec955
skfy <shiro218266@gmail.com>
2021-12-02 11:29:53 +0000

    fourth commit

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,2 +1,38 @@
 class AddressesController < ApplicationController
+
+  def create
+     address = Address.new(address_params)
+     address.customer_id = current_customer.id
+     address.save
+     redirect_to '/addresses'
+  end
+
+  def show
+  end
+
+  def index
+    @address = Address.new
+    @addresses = Address.all
+  end
+
+  def edit
+    @address = Address.find(params[:id])
+
+  end
+
+  def update
+    @address = Address.find(params[:id])
+    @address.update(address_params)
+    redirect_to addresses_path
+  end
+
+  def destroy
+  end
+
+  private
+  # ストロングパラメータ
+  def address_params
+    params.require(:address).permit(:postal_code, :address, :name)
+  end
+
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,7 +3,9 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   before_save { self.email.downcase! }
-  
+
+  has_many :addresses, dependent: :destroy
+
 end
