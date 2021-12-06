ba899b495f708a00b589f3f18d2323fa6093fd86
skfy <shiro218266@gmail.com>
2021-12-06 11:58:05 +0000

    seventh commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,43 +3,28 @@ Rails.application.routes.draw do
   devise_for :admin, controllers: {
   sessions: 'admin/sessions'
   }
-  
-  
-  
-  devise_for :customers
-  root to: 'homes#top'
-  get 'home/about' => 'homes#about'
 
   namespace :admin do
+    get 'home/admin' => 'homes#admin'
     resources :customers
-  end
-
-  namespace :admin do
     resources :genres
-  end
-
-  namespace :admin do
     resources :items
-  end
-
-  namespace :admin do
     resources :order_details
-  end
-
-  namespace :admin do
     resources :orders
   end
 
+  devise_for :customers
+  root to: 'homes#top'
+  get 'home/about' => 'homes#about'
   get 'customers/my_page' => 'customers#show'
   patch 'customers/withdraw' => 'customers#withdraw'
   get 'customers/unsubscribe' => 'customers#unsubscribe'
 
-
   resources :addresses
   resources :cart_items
   resources :customers
-
   resources :orders
+
   resources :items do
     #get 'items' => 'index'
     get 'order' => 'orders#show'
