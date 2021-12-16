43b743ad58c3fb408e91892319ae300fcdc085b9
skfy <shiro218266@gmail.com>
2021-12-16 13:03:50 +0000

    seventeenth commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
   attachment :image
   belongs_to :genre
   has_many :order_details, dependent: :destroy
+  belongs_to :order
 
   enum is_active: { on_sale: true, stop_selling: false }
 
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'a3efa1159d8237510e34cac105f801e00d6598628fbf3afa30baea21bf021f66fc52cdfd28fad2886b2bee97ea5ed12da1d021f56b6e480c5b5f7e73083d04e1'
