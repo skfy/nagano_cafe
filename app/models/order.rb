fe085c7eca9f12ad0b8c463d847c1091ecddaaa8
skfy <shiro218266@gmail.com>
2021-12-22 12:57:14 +0000

    21 commit

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,5 @@ class Order < ApplicationRecord
     has_many :items, dependent: :destroy
 
     enum payment_method: { credit_card: 0, transfer: 1 }
+    enum status: { waiting_for_deposit: 0, payment_confirmation: 1, production:2, ready_to_ship:3, sent:4 }
 end
