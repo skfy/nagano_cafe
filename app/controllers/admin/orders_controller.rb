d2b3e565de8be8383979aa2a34353caedef87606
skfy <shiro218266@gmail.com>
2022-01-08 12:59:00 +0000

    25 commit

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -4,4 +4,14 @@ class Admin::OrdersController < ApplicationController
     @customer = @order.customer
     @items = @order.order_details
   end
+  def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    redirect_to admin_order_path(@order)
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:status)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_04_083900) do
+ActiveRecord::Schema.define(version: 2022_01_08_125239) do
 
   create_table "addresses", force: :cascade do |t|
     t.string "name"
