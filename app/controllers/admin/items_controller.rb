63461e801dfadd80076010df5b8931fda7baadf9
skfy <shiro218266@gmail.com>
2022-01-13 11:56:30 +0000

    28 commit

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -11,7 +11,8 @@ class Admin::ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-     redirect_to '/admin/items'
+     redirect_to "/admin/items/#{@item.id}"
+     #redirect_to admin_item_path(@item)
     else
      render :new
     end
